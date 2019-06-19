c06178eb8b9107bbadcb22ab3055aa581efc2502
Bruce Collie <brucecollie82@gmail.com>
2019-06-19 09:25:28 +0100

    Completions

--- inhabited/schema.sql
+++ inhabited/schema.sql
@@ -1,7 +1,23 @@
 DROP TABLE IF EXISTS habit;
+DROP TABLE IF EXISTS completion;
 
+/*
+  The period of a habit is an integer value of days that specifies how often the
+  habit should be completed. For now, it's just a simple day count but in the
+  future it might be expanded to cover more complex recurrences.
+*/
 CREATE TABLE habit (
-  id      INTEGER PRIMARY KEY AUTOINCREMENT,
-  name    TEXT NOT NULL,
-  period  INTEGER NOT NULL
+  id              INTEGER PRIMARY KEY AUTOINCREMENT,
+  name            TEXT NOT NULL,
+  period          INTEGER NOT NULL
+);
+
+/*
+  The timestamp of a completion is a unix timestamp.
+*/
+CREATE TABLE completion (
+  id              INTEGER PRIMARY KEY AUTOINCREMENT,
+  timestamp       INTEGER NOT NULL,
+  habit_id        INTEGER NOT NULL,
+  FOREIGN KEY (habit_id) REFERENCES habit(id)
 );
