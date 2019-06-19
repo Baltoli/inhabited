ca8e861cbfc7158e0d5363993c899e60ef3b9e13
Bruce Collie <brucecollie82@gmail.com>
2019-06-19 09:32:58 +0100

    Add user table

--- inhabited/schema.sql
+++ inhabited/schema.sql
@@ -1,5 +1,12 @@
+DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS habit;
 DROP TABLE IF EXISTS completion;
+
+CREATE TABLE user (
+  id        INTEGER PRIMARY KEY AUTOINCREMENT,
+  username  TEXT UNIQUE NOT NULL,
+  password  TEXT NOT NULL
+);
 
 /*
   The period of a habit is an integer value of days that specifies how often the
@@ -7,17 +14,19 @@ DROP TABLE IF EXISTS completion;
   future it might be expanded to cover more complex recurrences.
 */
 CREATE TABLE habit (
-  id              INTEGER PRIMARY KEY AUTOINCREMENT,
-  name            TEXT NOT NULL,
-  period          INTEGER NOT NULL
+  id        INTEGER PRIMARY KEY AUTOINCREMENT,
+  name      TEXT NOT NULL,
+  period    INTEGER NOT NULL,
+  user_id   INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
 /*
   The timestamp of a completion is a unix timestamp.
 */
 CREATE TABLE completion (
-  id              INTEGER PRIMARY KEY AUTOINCREMENT,
-  timestamp       INTEGER NOT NULL,
-  habit_id        INTEGER NOT NULL,
+  id        INTEGER PRIMARY KEY AUTOINCREMENT,
+  timestamp INTEGER NOT NULL,
+  habit_id  INTEGER NOT NULL,
   FOREIGN KEY (habit_id) REFERENCES habit(id)
 );
