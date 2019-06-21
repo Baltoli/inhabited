c3d1a8a76b05e98de26bdea2f014d78a9bc724ca
Bruce Collie <brucecollie82@gmail.com>
2019-06-21 16:16:35 +0100

    Start to load habits

--- inhabited/habits.py
+++ inhabited/habits.py
@@ -9,9 +9,42 @@ from inhabited.db import get_db
 bp = Blueprint('habits', __name__)
 
 @bp.route('/')
+@login_required
 def index():
     db = get_db()
+
     habits = db.execute(
-        # query here
+        'SELECT id, name '
+        'FROM habit WHERE user_id = ?',
+        (g.user['id'],)
     ).fetchall()
+
     return render_template('habits/index.html', habits=habits)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        period = request.form['period']
+        error = None
+
+        if not name:
+            error = 'Name is required'
+
+        if not period:
+            error = 'Period is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO habit (name, period, user_id) '
+                'VALUES (?, ?, ?)',
+                (name, period, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('habits.index'))
+
+    return render_template('habits/create.html')
