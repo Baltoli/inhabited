a4beb4206d738923c74872a5e4f85822d64e9f51
Bruce Collie <brucecollie82@gmail.com>
2019-06-24 09:53:32 +0100

    Start to work on editing etc

--- inhabited/habits.py
+++ inhabited/habits.py
@@ -48,3 +48,49 @@ def create():
             return redirect(url_for('habits.index'))
 
     return render_template('habits/create.html')
+
+def get_habit(id):
+    habit = get_db().execute(
+        'SELECT h.id, name, period, user_id, u.username '
+        ' FROM habit h JOIN user u ON h.user_id = u.id'
+        ' WHERE h.id = ?',
+        (id,)
+    ).fetchone()
+
+    if habit is None:
+        abort(404, "Habit id {0} doesn't exist".format(id))
+
+    if habit['user_id'] != g.user['id']:
+        abort(403)
+
+    return habit
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    habit = get_habit(id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        period = request.form['period']
+        error = None
+
+        if not name:
+            error = 'Name is required'
+
+        if not period:
+            error = 'Period is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE habit SET name = ?, period = ?'
+                ' WHERE id = ?',
+                (name, period, id)
+            )
+            db.commit()
+            return redirect(url_for('habits.index'))
+
+    return render_template('habits/update.html', habit=habit)
